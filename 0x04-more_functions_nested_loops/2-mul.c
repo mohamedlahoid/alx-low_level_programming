40ad5f3ad20871e5353cda5c2e50256d319d30ac
mohamedlahoid <lahoidmohamed4@gmail.com>
2023-08-17 07:00:59 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Multuplise two intiger
+ * @x: the first number
+ * @y: th second number
+ * Return: the result of between
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
