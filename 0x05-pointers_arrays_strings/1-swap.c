e6119f27b564f61ccb322d4e7e06e17e6282e5d5
mohamedlahoid <lahoidmohamed4@gmail.com>
2023-08-21 09:15:23 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
+ * main - Entry point
  * swap int - swap the value of two intigers
- * @a: input para
- * @b: input para
- * Return: nothing
+ * @a: input parameter
+ * @b: input parameter
+ * * Return: nothing
  */
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b) /* the function that swaps the value */
 {
 	int m;
+
 	m = *a;
 	*a = *b;
 	*b = m;
