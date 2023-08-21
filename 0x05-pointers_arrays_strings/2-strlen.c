b6b15c35eca7edd9cc93f297d7c7c8963762c8c9
mohamedlahoid <lahoidmohamed4@gmail.com>
2023-08-21 09:41:35 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - return the legnth
+ * @s: input paramter
+ * Return: legnth of string
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\n'; s++;
+			++counter;
+
+	return (counter);
+}
+
