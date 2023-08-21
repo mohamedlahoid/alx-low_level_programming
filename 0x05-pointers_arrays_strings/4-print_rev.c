6c87588c503be33419b742e41a0463cd866ae468
mohamedlahoid <lahoidmohamed4@gmail.com>
2023-08-21 11:02:17 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print rev - print reversed string
+ * @s: pointer to the string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int start = 0;
+
+	while (s[start])
+		start++;
+	while (start--)
+		_putchar(s[start]);
+	_putchar('\n');
+}
