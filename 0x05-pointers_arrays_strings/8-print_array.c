12fca14ba72f020ad2e186f0a7ed9fca78351155
mohamedlahoid <lahoidmohamed4@gmail.com>
2023-08-21 12:04:52 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,15 +3,16 @@
  * print_aray - print element
  * @n: element par
  * @a: string par
+ * Return: void
  */
 void print_array(int *a, int n)
 {
-	int index_of_array;
+	int j;
 
-	for (index_of_array = 0; index_of_array < n; index_of_array++)
+	for (j = 0; j < n; j++)
 	{
-		printf("%d", a[index_of_array]);
-		if (index_of_array != (n - 1))
+		printf("%d", a[j]);
+		if (j != (n - 1))
 		{
 			printf(", ");
 		}
