08487d0477c0b102f81fef9cb2bf90215f6ad7ec
mohamedlahoid <lahoidmohamed4@gmail.com>
2023-08-28 08:24:28 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for any set of byte
+ * @s: string
+ * @accept:string to match
+ * Return: pointer to the byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int itr, jtr;
+	char *p;
+
+	itr = 0;
+	while (s[itr] != '\0')
+	{
+		jtr = 0;
+		while (accept[jtr] != '\0')
+		{
+			if (accept[jtr] == s[itr])
+			{
+				p = &s[itr];
+				return (p);
+			}
+			jtr++;
+		}
+		itr++;
+	}
+	return (0);
+}
